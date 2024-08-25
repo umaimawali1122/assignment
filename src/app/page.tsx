3076ff5a188c0f31d66ad956e7b8c3c5b7d8742c
umaimawali1122 <umaimawali111@gmail.com>
2024-08-25 22:12:48 +0500

    updated code for project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
-const page = () => {
-  return(
-     <div>home pg</div>
-  )
-}
+const Testimonial = () => {
+  return (
+    <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+      <Image 
+        className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" 
+        src="/umaima.png" 
+        alt="umaima wali" 
+        width={384} 
+        height={512} 
+        priority // This ensures the image is loaded immediately for better LCP
+      />
+      <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+        <blockquote>
+          <p className="text-lg font-medium">
+            {`“Tailwind CSS is the only framework that I've seen scale
+            on large teams. It’s easy to customize, adapts to any design,
+            and the build size is tiny.”`}
+          </p>
+        </blockquote>
+        <figcaption className="font-medium">
+          <div className="text-sky-500 dark:text-sky-400">
+            umaima wali
+          </div>
+          <div className="text-slate-700 dark:text-slate-500">
+            Staff Engineer
+          </div>
+        </figcaption>
+      </div>
+    </figure>
+  );
+};
 
-export default page
+export default Testimonial;
